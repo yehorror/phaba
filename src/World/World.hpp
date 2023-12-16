f746bfaf3f04a0fdfc456f0dc330f2719440647c
Yehor Sorokoladov <i@yehor.dev>
2023-12-16 13:52:09 +0200

    Create World class, add some dummy test

---
+++ src/World/World.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Phaba
+{
+    class World
+    {
+    public:
+
+    private:
+
+    };
+}
---
+++ test/WorldTest.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "World/World.hpp"
+
+TEST(WorldTest, WorldCanBeCreated)
+{
+    Phaba::World world;
+}
