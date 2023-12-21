2b7181bee355cdc4301ad2d504b89bd8935ca737
Yehor Sorokoladov <i@yehor.dev>
2023-12-21 23:50:59 +0200

    Move GL context to separate class in test main

--- test/Main.cpp
+++ test/Main.cpp
@@ -4,37 +4,77 @@
 #include <SDL2/SDL.h>
 #include <GL/glew.h>
 
+// This context is required to run tests which involves gl* functions call
+// Need to initialize SDL, create window (can be hidden), GL context, and glew
+// This class is responsible to keep this context until tests stops
+class GLContext
+{
+public:
+    using WindowGuard = std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)>;
+    using ContextGuard = std::unique_ptr<std::remove_pointer_t<SDL_GLContext>, decltype(&SDL_GL_DeleteContext)>;
+
+    GLContext()
+        : m_window(nullptr, &SDL_DestroyWindow)
+        , m_glContext(nullptr, &SDL_GL_DeleteContext)
+    {
+        if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        {
+            throw std::runtime_error("Failed to init SDL: " + std::string(SDL_GetError()));
+        }
+
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+        m_window = WindowGuard(
+            SDL_CreateWindow(
+                "Whatevs",
+                SDL_WINDOWPOS_CENTERED,
+                SDL_WINDOWPOS_CENTERED,
+                800,
+                600,
+                SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN),
+            &SDL_DestroyWindow
+        );
+
+        if (!m_window)
+        {
+            throw std::runtime_error("Failed to create invisible window");
+        }
+
+        m_glContext = ContextGuard(
+            SDL_GL_CreateContext(m_window.get()),
+            &SDL_GL_DeleteContext
+        );
+
+        const GLenum glewInitResult = glewInit();
+        if (glewInitResult != GLEW_OK)
+        {
+            throw std::runtime_error("Failed to initialize glew");
+        }
+    }
+
+    ~GLContext()
+    {
+        SDL_Quit();
+    }
+
+private:
+    WindowGuard m_window;
+    ContextGuard m_glContext;
+};
+
 int main(int argc, char** argv)
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    try
     {
-        throw std::runtime_error("Failed to init SDL: " + std::string(SDL_GetError()));
+        GLContext context;
+
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
     }
-
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-
-    const auto windowHandle = SDL_CreateWindow(
-        "Whatevs",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN);
-
-    if (!windowHandle)
+    catch (const std::exception& e)
     {
+        std::cerr << "Failed to run tests: " << e.what() << std::endl;
         return -1;
     }
-
-    auto glContext = SDL_GL_CreateContext(windowHandle);
-
-    const GLenum glewInitResult = glewInit();
-    if (glewInitResult != GLEW_OK)
-    {
-        return -1;
-    }
-
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
