61ef3fbde11cc4e7c494fbc04f0b4414a5329cf3
Yehor Sorokoladov <i@yehor.dev>
2024-01-02 21:50:11 +0200

    Make totally uncompilable test for bouncing off of the floor

--- test/WorldTest.cpp
+++ test/WorldTest.cpp
@@ -72,3 +72,58 @@ TEST(WorldTest, FreeFallAcceleration_StaticBody_TwoSecondsStep_PositionOfBodyHas
 
     EXPECT_EQ(body.GetPosition(), Phaba::Vector2(0.0f, 0.0f));
 }
+
+TEST(WorldTest, FreeFallAcceleration_SceneHasFloorAndABox_BoxBouncesOffOfTheFloor)
+{
+    /*
+                ***
+                ***     <- it should 'bounce'-off of the floor
+
+        ***********************
+
+    */
+    constexpr Phaba::TimeDelta k100ms = 0.100f;
+    constexpr Phaba::Vector2 kGAcceleration = { 0.0f, -2.f };
+
+    Phaba::World world(kGAcceleration);
+
+    constexpr glm::vec2 s_floorVertices[] = {
+        {-5.f,  0.2f},
+        { 5.f,  0.2f},
+        { 5.f, -0.2f},
+        {-5.f, -0.2f}
+    };
+
+    constexpr glm::vec2 s_boxVertices[] = {
+        {-1.0,  1.0},
+        { 1.0,  1.0},
+        { 1.0, -1.0},
+        {-1.0, -1.0}
+    };
+
+    auto floorPart = world.CreateBodyPart(s_floorVertices);
+    auto floor = world.CreateBody({
+        .part = floorPart,
+        .type = Phaba::BodyType::kStatic,
+        .position = Phaba::Vector2{0.f, -1.f}
+        }
+    );
+
+    auto boxPart = world.CreateBodyPart(s_boxVertices);
+    auto box = world.CreateBody({
+        .part = boxPart,
+        .type = Phaba::BodyType::kDynamic,
+        .position = Phaba::Vector2{0.f, 0.5f}
+        }
+    );
+
+    ASSERT_EQ(box.GetVelocity(), Phaba::Vector2(0.0f, 0.0f));
+
+    for (int i = 0; i < 10; ++i)
+    {
+        world.Step(k100ms);
+    }
+
+    const auto velocityAfterFall = box.GetVelocity();
+    EXPECT_GT(velocityAfterFall.y, 0.f);
+}
